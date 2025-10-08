e7b3d50e0576cbc182b2c105308d57cbcda960b7
Marics <your-username@users.noreply.github.com>
2025-10-08 14:46:49 +0200

    Updated UserGymMembership

--- src/Domain/Entities/UserGymMembership.cs
+++ src/Domain/Entities/UserGymMembership.cs
@@ -2,7 +2,8 @@ namespace FitPass.Domain.Entities;
 
 public class UserGymMembership : BaseEntity
 {
-    public required string UserId { get; set; }
+    public required string? UserId { get; set; }
+    public required string? ApplicationUserId { get; set; }
     public required string? GymId { get; set; }
     public GymMembershipStatus GymMembershipStatus { get; set; } = GymMembershipStatus.Member;
     public DateTimeOffset? MemberSince { get; set; } = DateTimeOffset.UtcNow;
